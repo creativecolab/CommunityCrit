7847ed49331df53d13509f93869add1c104d550e
Joseph Le <josephle@me.com>
2017-06-27 16:25:08 -0700

    Update homepage text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,14 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    CommunityCrit allows for the public to learn about urban design proposals and give feedback.
+                </div>
+                
+                <div class="panel-footer text-right">
+                    <a href="#" class="btn btn-primary">Get Started</a>
                 </div>
             </div>
         </div>
