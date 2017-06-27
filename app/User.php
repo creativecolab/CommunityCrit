b1721ffede43a98dba3a4a40929f810e1e3841f8
Joseph Le <josephle@me.com>
2017-06-27 16:37:05 -0700

    Add condition constants to User model

--- app/User.php
+++ app/User.php
@@ -7,6 +7,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+	const CONDITION_GENERIC_HOLISTIC = 0;
+	const CONDITION_GENERIC_MICROTASK_OPEN = 1;
+	const CONDITION_GENERIC_MICROTASK_CLOSED = 2;
+	const CONDITION_PERSONAL_HOLISTIC = 3;
+	const CONDITION_PERSONAL_MICROTASK_OPEN = 4;
+	const CONDITION_PERSONAL_MICROTASK_CLOSED = 5;
+	const CONDITION_MIN = 0;
+	const CONDITION_MAX = 5;
+
     use Notifiable;
 
     /**
@@ -38,5 +47,21 @@ class User extends Authenticatable
 
     protected function getNameAttribute() {
     	return $this->fname . ' ' . $this->lname;
+    }
+
+	/**
+	 * Returns available conditions
+	 *
+	 * @return array
+	 */
+    public function getConditions() {
+    	return [
+    	    'GENERIC_HOLISTIC' => static::CONDITION_GENERIC_HOLISTIC,
+	        'GENERIC_MICROTASK_OPEN' => static::CONDITION_GENERIC_MICROTASK_OPEN,
+	        'GENERIC_MICROTASK_CLOSED' => static::CONDITION_GENERIC_MICROTASK_CLOSED,
+	        'PERSONAL_HOLISTIC' => static::CONDITION_PERSONAL_HOLISTIC,
+	        'PERSONAL_MICROTASK_OPEN' => static::CONDITION_PERSONAL_MICROTASK_OPEN,
+	        'PERSONAL_MICROTASK_CLOSED' => static::CONDITION_PERSONAL_MICROTASK_CLOSED,
+	    ];
     }
 }
