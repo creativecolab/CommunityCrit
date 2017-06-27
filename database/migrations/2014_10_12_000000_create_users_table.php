eb10eb26ea655d9ffbc600e6b184c21f295cc42b
Joseph Le <josephle@me.com>
2017-06-27 15:48:22 -0700

    Update User model fields
    Add condition and split name into fname and lname

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('fname');
+            $table->string('lname');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->tinyInteger('condition')->nullable();
             $table->timestamps();
         });
     }
