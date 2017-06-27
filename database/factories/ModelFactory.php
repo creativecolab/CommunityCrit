2f65593a030f2637de86c3d2fedf93ef6b61b058
Joseph Le <josephle@me.com>
2017-06-27 16:42:19 -0700

    Add condition to User factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,6 +21,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+	    'condition' => rand(App\User::CONDITION_MIN, App\User::CONDITION_MAX),
     ];
 });
 
