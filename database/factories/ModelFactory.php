be288677000563b91fe42275231f59b4e429a05d
Joseph Le <josephle@me.com>
2017-06-27 15:56:39 -0700

    Add Task factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,10 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Task::class, function (Faker\Generator $faker) {
+	return [
+		'name' => $faker->word,
+		'text' => $faker->paragraphs()
+	];
+});
