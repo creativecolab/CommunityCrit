fc4ab75c0fe053537ae9b72b89de8b5840764036
Joseph Le <josephle@me.com>
2017-06-30 16:01:55 -0700

    Add admin flag to User model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->tinyInteger('condition')->nullable();
+	        $table->boolean('admin')->default(false);
             $table->timestamps();
         });
     }
