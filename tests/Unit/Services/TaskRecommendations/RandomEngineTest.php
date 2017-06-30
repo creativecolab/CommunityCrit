b2ab2de5727582ad3809dd1b63011d87d23d98da
Joseph Le <josephle@me.com>
2017-06-29 17:26:14 -0700

    Add tests for recommendation engine

---
+++ tests/Unit/Services/TaskRecommendations/RandomEngineTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\Services\TaskRecommendations;
+
+use app\Services\TaskRecommendations\Engine;
+use App\Task;
+use App\User;
+use Illuminate\Support\Collection;
+use Tests\TestCase;
+use App\Services\TaskRecommendations\RandomEngine;
+
+class RandomEngineTest extends TestCase
+{
+	/* @var Engine */
+	private $engine;
+
+	public function setUp()
+	{
+		parent::setUp();
+		$this->engine = new RandomEngine();
+	}
+
+	public function testRecommend()
+	{
+		$user  = factory( User::class )->make();
+		$tasks = factory( Task::class, 10 )->make();
+
+		/* @var Collection $recommendations */
+		$recommendations = $this->engine->recommend( $user, $tasks );
+
+		$this->assertInstanceOf(Collection::class, $recommendations);
+		$this->assertTrue($recommendations->isNotEmpty());
+	}
+}
