313988ff1891542330284b4b9c60f4c850d25fb3
Joseph Le <josephle@me.com>
2017-06-30 16:18:52 -0700

    Add Admin middleware and routes

--- routes/web.php
+++ routes/web.php
@@ -11,25 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/', function () {
+	return view( 'welcome' );
+} );
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get( '/home', 'HomeController@index' )->name( 'home' );
 Route::group( [ 'prefix' => 'tasks' ], function () {
 	Route::get( '/', 'TaskController@index' );
 	Route::post( '{task}/feedback', 'TaskController@storeFeedback' );
 } );
-
-Route::group([
-	'prefix' => config('backpack.base.route_prefix', 'admin'),
-	'middleware' => ['admin'],
-	'namespace' => 'Admin'
-], function() {
-	// your CRUD resources and other admin routes here
-	CRUD::resource('feedback', 'FeedbackCrudController');
-	CRUD::resource('tasks', 'TaskCrudController');
-	CRUD::resource('users', 'UserCrudController');
-});
