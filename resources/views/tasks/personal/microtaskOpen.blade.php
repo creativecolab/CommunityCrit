3370ace7a6a8f4909a9bab5b3d4d6a19746c3313
Reggie Wu <reggiewumo@gmail.com>
2017-07-05 16:31:46 +0000

    recommended at top of page for MTO closed #12

--- resources/views/tasks/personal/microtaskOpen.blade.php
+++ resources/views/tasks/personal/microtaskOpen.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    {{--Could be made more efficient--}}
     @foreach($tasks as $task)
+        @if($recommendations->contains($task->id))
         @component('tasks.common.microtask', ['id' => $task->id, 'subtasks' => $task->getImmediateDescendants(), 'recommended' => $recommendations->contains($task->id), 'recommendations' => $recommendations])
             @slot('title')
                 {{ $task->name }}
@@ -11,5 +13,19 @@
                 {!! html_entity_decode($task->text) !!}
             @endslot
         @endcomponent
+        @endif
+    @endforeach
+    @foreach($tasks as $task)
+        @if(!($recommendations->contains($task->id)))
+            @component('tasks.common.microtask', ['id' => $task->id, 'subtasks' => $task->getImmediateDescendants(), 'recommended' => $recommendations->contains($task->id), 'recommendations' => $recommendations])
+            @slot('title')
+            {{ $task->name }}
+            @endslot
+
+            @slot('text')
+            {!! html_entity_decode($task->text) !!}
+            @endslot
+            @endcomponent
+        @endif
     @endforeach
 @endsection
