2404c9ceab20c5cfd38b6c7a5f7fa828412208a1
vagrant <vagrant@homestead>
2017-07-05 16:25:31 +0000

    fix duplicate tasks in personal holistic

--- resources/views/tasks/personal/holistic.blade.php
+++ resources/views/tasks/personal/holistic.blade.php
@@ -2,24 +2,26 @@
 
 @section('content')
     @foreach($tasks as $task)
-        <div class="row">
-            <div class="col-xs-12">
-                <h2>
-                    {{ $task->name }}
-                    <small>@if($recommendations->contains($task->id)) <span
-                                class="label label-primary">Recommended for You</span>@endif</small>
-                </h2>
+        @if($task->depth == 1)
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2>
+                        {{ $task->name }}
+                        <small>@if($recommendations->contains($task->id)) <span
+                                    class="label label-primary">Recommended for You</span>@endif</small>
+                    </h2>
 
-                <p>{!! html_entity_decode($task->text) !!}</p>
+                    <p>{!! html_entity_decode($task->text) !!}</p>
 
-                @foreach($task->getImmediateDescendants() as $subtask)
-                    <strong>{{ $subtask->name }}@if($recommendations->contains($subtask->id)) <span
-                                class="label label-primary">Recommended for You</span>@endif</strong>
+                    @foreach($task->getImmediateDescendants() as $subtask)
+                        <strong>{{ $subtask->name }}@if($recommendations->contains($subtask->id)) <span
+                                    class="label label-primary">Recommended for You</span>@endif</strong>
 
-                    <p>{!! html_entity_decode($subtask->text) !!}</p>
-                @endforeach
+                        <p>{!! html_entity_decode($subtask->text) !!}</p>
+                    @endforeach
+                </div>
             </div>
-        </div>
+        @endif
     @endforeach
 
     <div class="row">
