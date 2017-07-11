7be482215d60c449af19351b95c22d5c6806a160
Reggie Wu <reggiewumo@gmail.com>
2017-07-11 23:38:02 +0000

    add code for collapsible functionality

--- resources/views/tasks/facets/singleFacet.blade.php
+++ resources/views/tasks/facets/singleFacet.blade.php
@@ -19,6 +19,20 @@
     </blockquote>
     @endforeach
 
+    {{--collapsible--}}
+    {{--<div id="accordion" role="tablist">--}}
+        {{--@foreach ($tasks as $task)--}}
+            {{--<p>{{ $task->quote }}--}}
+                {{--<a role="button" data-toggle="collapse" data-parent="#accordion" href="#{{$task->id}}" aria-expanded="true" aria-controls="collapseOne">(see original text)</a></p>--}}
+            {{--<div id="{{$task->id}}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">--}}
+                {{--<blockquote>--}}
+                    {{--<p>{!! $task->text !!}</p>--}}
+                    {{--<footer><a href="{{ action('TaskController@singleSource', $task->source->slug) }}">{{ $task->source->name }}</a></footer>--}}
+                {{--</blockquote>--}}
+            {{--</div>--}}
+        {{--@endforeach--}}
+    {{--</div>--}}
+
     @component('tasks.commentsPage', [ 'task' => $facet ])
     @endcomponent
 @endsection
