a3260347fdbccf4c8b614333dd12d3c2eeeeff65
michelleng <michelleng@college.harvard.edu>
2017-07-13 15:33:42 -0700

    Updated "Who We Are"

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,7 +11,7 @@
                 <p>CommunityCrit allows the public to learn about urban design proposals and give feedback. [more about the platform]</p>
 
                 <h2>Who We Are</h2>
-                <p>The following are extracts from Chace's narrative: "Every fact seemed to warrant me in concluding that it was anything but chance which directed his operations; he made two several attacks upon the ship, at a short interval between them, both of which, according to their direction, were calculated to do us the most injury, by being made ahead, and thereby combining the speed of the two objects for the shock; to effect which, the exact manoeuvres which he made were necessary. His aspect was most horrible, and such as indicated resentment and fury. He came directly from the shoal which we had just before entered, and in which we had struck three of his companions, as if fired with revenge for their sufferings." Again: "At all events, the whole circumstances taken together, all happening before.</p>
+                <p>We are a team of researchers in the Design Lab at the University of California San Diego. Our goal is to develop technologies that enable people to share their ideas about designing the future of their communities. We hope that our online tool, CommunityCrit, will facilitate community participation and supplement traditional face-to-face workshops to get broader feedback on urban design projects.</p><p>We have chosen the recent planning efforts around the extension of the 14th Street Promenade in the East Village neighborhood of downtown San Diego as our test case.</p>
 
                 <h2>Our Partners</h2>
                 [our partners]
