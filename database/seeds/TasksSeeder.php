04d19a61a7954d45ec82a2939c14fa1088dd7dd4
michelleng <michelleng@college.harvard.edu>
2017-08-01 15:17:31 -0700

    about text changes

--- database/seeds/TasksSeeder.php
+++ database/seeds/TasksSeeder.php
@@ -18,68 +18,63 @@ class TasksSeeder extends Seeder
         ],
         [
             'name' => 'Submit an Idea',
-            'text' => 'What would you like to see or do at El Nudillo, the intersection of 14th Street and National Avenue?',
+            'text' => 'What would you like to see or do at El Nudillo, the intersection of 14th Street and National Avenue, by yourself or with family and friends?',
             'type' => '80',
         ],
-        [
-            'name' => 'Raise a Related Issue',
-            'text' => 'What issues do you think relate to this idea?',
-            'type' => '91',
-        ],
+//        [
+//            'name' => 'Raise an Issue',
+//            'text' => 'What issues relate to this idea?',
+//            'type' => '91',
+//        ],
         [
             'name' => 'Add a Story',
-            'text' => 'Tell a story that relates to this idea.',
+            'text' => 'Do you have a story or personal experience related to this idea?',
             'type' => '91',
         ],
         [
             'name' => 'Add an Example',
-            'text' => 'Describe something that already exists that relates to this idea.',
+            'text' => 'Can you think of any examples (such as a place or an object) that could inspire the design of this idea?',
             'type' => '91',
         ],
         [
             'name' => 'Improve the Idea',
-            'text' => 'Improve the current idea by suggesting changes or additions that support the reference.',
+            'text' => 'How would you improve this idea based on the above reference?',
             'type' => '91',
         ],
         [
-            'name' => 'Evaluate Feasibility',
-            'text' => 'How feasible is this idea?',
+            'name' => 'Evaluate Feasability',
+            'text' => 'Do you think this idea is feasible? Why or why not?',
             'type' => '101',
         ],
-        [
-            'name' => 'Evaluate Benefit',
-            'text' => 'How might this idea benefit the community?',
-            'type' => '101',
-        ],
-        [
-            'name' => 'Evaluate Values',
-            'text' => 'How does this idea relate to the values of the surrounding community?',
-            'type' => '101',
-        ],
+//        [
+//            'name' => 'Evaluate Benefit',
+//            'text' => 'How might this idea benefit the community?',
+//            'type' => '101',
+//        ],
+//        [
+//            'name' => 'Evaluate Values',
+//            'text' => 'How well does this idea align with the values of the community?',
+//            'type' => '101',
+//        ],
         [
             'name' => 'Evaluate Impact: Safety',
-            'text' => 'How might this idea impact safety in this area?',
+            'text' => 'How do you think this idea might impact safety in the area?',
             'type' => '101',
         ],
         [
-            'name' => 'Evaluate Impact: Traffic',
-            'text' => 'How might this idea impact traffic in this area?',
+            'name' => 'Evaluate Impact: Mobility',
+            'text' => 'How do you think this idea might impact mobility—from walking or biking to driving and public transit—in the area?',
             'type' => '101',
         ],
         [
             'name' => 'Evaluate Impact: Quality of Life',
-            'text' => 'How might this idea impact quality of life in this area?',
+            'text' => 'How do you think this idea might impact quality of life in the area?',
             'type' => '101',
         ],
         [
             'name' => 'Critique the Idea',
-            'text' => 'How well does the current idea align with this reference?',
+            'text' => 'The above reference provides a guideline for the design of this idea. What do you think of this idea based on the reference?',
             'type' => '102',
-        ],
-        [
-            'name' => 'Rate the Idea',
-            'text' => 'Rate the idea across the criteria below.',
-            'type' => '100',
         ],
     ];
     /**
