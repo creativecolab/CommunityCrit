5884d9028961077f50ab24579e4fb03a2a98903d
Michael James <thelivingpc@gmail.com>
2017-08-02 14:13:37 -0700

    modified Ideas table, included image in ideas task component
    - added “img_url” to ideas table
    - changed “text” from string to text on ideas table
    - added logic to show/hide image from idea component if null
    - added in image and switched to floats

--- resources/views/activities/common/idea.blade.php
+++ resources/views/activities/common/idea.blade.php
@@ -1,16 +1,24 @@
-<div class="row">
-    <!-- <div class="col-md-3"> -->
-        <!-- <img class="img-responsive" src="/img/placeholder.jpg"></img> -->
-    <!-- </div> -->
-    <!-- <div class="col-md-9"> -->
+<div class="row" style="margin-bottom: 20px;">
     <div class="col-md-12">
-        <h2 class="no-marg-top">
-        Idea<!-- 
-        -->@if ($idea->name)<!-- 
-             -->: {!! $idea->name !!}
+        @if ($idea->img_url)
+            <div style="float: left; margin-right: 22px; max-width: 30%;">
+            <!-- <div class="col-md-3"> -->
+                <img class="img-responsive" src="{!! $idea->img_url !!}" style="max-height: 250px;"></img>
+            <!-- </div> -->
+            <!-- <div class="col-md-9"> -->
+            </div>
+            <div style="float: left; max-width: 60%;">
+        @else
+            <div style="float: left; max-width: 100%;">
         @endif
-        <!-- <button class="btn btn-default pull-right">Switch idea</button> -->
-        </h2>
-        <p style="font-size: 120%;">{!! $idea->text !!}</p>
+            <h2 class="no-marg-top">
+            Idea<!-- 
+            -->@if ($idea->name)<!-- 
+                 -->: {!! $idea->name !!}
+            @endif
+            <!-- <button class="btn btn-default pull-right">Switch idea</button> -->
+            </h2>
+            <p style="font-size: 120%;">{!! $idea->text !!}</p>
+        </div>
     </div>
 </div> <!-- .row -->
