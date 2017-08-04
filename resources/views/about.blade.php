eb1e34d4ddd0d2316261b45e6be9d26e38cdbcf0
michelleng <michelleng@college.harvard.edu>
2017-08-04 16:20:29 -0700

    added password instructions

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,8 +7,13 @@
         <h1>About</h1>
     </div>
     	<h3>14th Street Promenade Team</h3>
-    	<p>The 14th Street Promenade team has helped develop the East Village South Draft Focus Plan and the 14th Street Master Plan. Their members include Roger Lewis, President of C3, and Beth Callendar, the incoming Chair of the Urban Land Institute San Diego/Tijuana District Council.</p><p>The 14th Street Promenade team is now beginning to design certain parts of the 14th Street Promenade, including El Nudillo.</p>
+    	<p>The 14th Street Promenade team helped develop the East Village South Draft Focus Plan and the 14th Street Master Plan. Their members include Roger Lewis, President of C3, Beth Callendar, the incoming Chair of the Urban Land Institute's San Diego/Tijuana District Council, residents of East Village, and architects and city planners from around San Diego.</p><p>The 14th Street Promenade team is now beginning to design specific parts of the 14th Street Promenade, including El Nudillo.</p>
 
     	<h3>CommunityCrit</h3>
         <p>We are a team of researchers in the Design Lab at the University of California San Diego, led by Professor Steven Dow and Dr. Narges Mahyar. Our goal is to develop technologies that enable people to co-design the future of their communities.</p>We have created CommunityCrit (short for Community Critique) to facilitate community participation, supplement traditional face-to-face workshops, and collect broader feedback on urban design projects. We have chosen the recent planning efforts around the 14th Street Promenade in the East Village neighborhood of downtown San Diego as our test case.</p>
+
+        <h3>Partners</h3>
+        <p>Roger Lewis, President of C3</p>
+        <p>Beth Callendar, incoming Chair of the Urban Land Institute's San Diego/Tijuana District Council</p>
+
 @endsection
