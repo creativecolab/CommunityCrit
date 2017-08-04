15ffd6f808a2538481d2f42b3ac8e2b256cd66b5
michelleng <michelleng@college.harvard.edu>
2017-08-04 16:54:31 -0700

    added image to landing page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,7 +13,7 @@
         <p>We are a team of researchers in the Design Lab at the University of California San Diego, led by Professor Steven Dow and Dr. Narges Mahyar. Our goal is to develop technologies that enable people to co-design the future of their communities.</p>We have created CommunityCrit (short for Community Critique) to facilitate community participation, supplement traditional face-to-face workshops, and collect broader feedback on urban design projects. We have chosen the recent planning efforts around the 14th Street Promenade in the East Village neighborhood of downtown San Diego as our test case.</p>
 
         <h3>Partners</h3>
-        <p>Roger Lewis, President of C3</p>
-        <p>Beth Callendar, incoming Chair of the Urban Land Institute's San Diego/Tijuana District Council</p>
+        <p>Citizens Coordinate for Century 3</p>
+        <p>East Village Group</p>
 
 @endsection
