6ae7792396c081065bb7371f822f41abea5cd657
michelleng <michelleng@college.harvard.edu>
2017-08-07 16:35:45 -0700

    added specific questions to questionsSeeder

--- database/seeds/QuestionsSeeder.php
+++ database/seeds/QuestionsSeeder.php
@@ -12,8 +12,26 @@ class QuestionsSeeder extends Seeder
      */
 	public $data = [
 	    [
-            'text' => 'What artists would you recommend to create this artwork?',
+            'text' => 'What color(s) would you want a tower at El Nudillo to be?',
+			'idea_id' => 1,
+			'user_id' => 3,
+			'status' => 1,
+        ],
+	    [
+            'text' => 'What artist(s) would you recommend to create artwork at El Nudillo?',
 			'idea_id' => 2,
+			'user_id' => 3,
+			'status' => 1,
+        ],
+    	[
+            'text' => 'How many crosswalks do you think a roundabout at El Nudillo should have?',
+			'idea_id' => 3,
+			'user_id' => 3,
+			'status' => 1,
+        ],
+        [
+            'text' => 'Do you think people should be able to play in a fountain at El Nudillo? Why or why not?',
+			'idea_id' => 4,
 			'user_id' => 3,
 			'status' => 1,
         ],
