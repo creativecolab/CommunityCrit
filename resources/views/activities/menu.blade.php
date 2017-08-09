07a76500d8856b02529b3943251da5c44b5d260e
Reggie Wu <reggiewumo@gmail.com>
2017-08-09 23:06:35 +0000

    simple summary page

--- resources/views/activities/menu.blade.php
+++ resources/views/activities/menu.blade.php
@@ -32,9 +32,9 @@
             Submit a New Idea
         </a>
 
-        <h2>All done?</h2>
-        <a class="btn btn-primary" href="{{ route( 'exit') }}">
-            Go to Exit Survey
-        </a>
+        {{--<h2>All done?</h2>--}}
+        {{--<a class="btn btn-primary" href="{{ route( 'exit') }}">--}}
+            {{--Go to Exit Survey--}}
+        {{--</a>--}}
     </div>
 @endsection
