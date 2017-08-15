8945136c0100b1e750f2b352a6966ff04ce67c10
Michael James <thelivingpc@gmail.com>
2017-08-15 12:28:25 -0700

    add user attributes to Google Analytics

--- resources/views/utilities/analytics.blade.php
+++ resources/views/utilities/analytics.blade.php
@@ -4,13 +4,19 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  var myID = '';
+  var myID = '', admin = 0, type = 0, created_at = '';
   @if (!(Auth::guest()))
 	myID = '{{ Auth::user()->id }}';
+  admin = {{ Auth::user()->admin }};
+  type = {{ Auth::user()->type }};
+  created_at = '{{ Auth::user()->created_at }}';
   @endif
 
   ga('create', 'UA-104110133-1', 'auto', {
-  	userId: myID
+  	userId: myID,
+    dimension2: admin,
+    dimension3: type,
+    dimension4: created_at
   });
   ga('send', 'pageview');
 
