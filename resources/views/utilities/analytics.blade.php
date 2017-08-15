82444e772f13c5a4875b0cbaae323ae537578553
Michael James <thelivingpc@gmail.com>
2017-08-15 14:33:16 -0700

    add additional variables to google analytics

--- resources/views/utilities/analytics.blade.php
+++ resources/views/utilities/analytics.blade.php
@@ -4,20 +4,27 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  var myID = '';
+  var myID = '', admin = 0, type = 0, created_at = '';
   @if (!(Auth::guest()))
-	myID = '{{ Auth::user()->id }}';
+  myID = '{{ Auth::user()->id }}';
+  admin = {{ Auth::user()->admin }};
+  type = {{ Auth::user()->type }};
+  created_at = '{{ Auth::user()->created_at }}';
   @endif
 
   ga('create', 'UA-104110133-1', 'auto', {
-  	userId: myID
+    userId: myID,
+    dimension1: myID,
+    dimension2: admin,
+    dimension3: type,
+    dimension4: created_at
   });
   ga('send', 'pageview');
 
   // ga('create', 'UA-104110133-1', 'auto');
   // ga('send', 'pageview');
   {{--@if (!(Auth::guest()))--}}
-  	{{--// ga('set', 'userId', '{{ Auth::user()->id }}'); // Set the user ID using signed-in user_id.--}}
+    {{--// ga('set', 'userId', '{{ Auth::user()->id }}'); // Set the user ID using signed-in user_id.--}}
   {{--@endif--}}
 
 </script>
