0371ae670275d8946700380b3788d58c6ea8b546
Reggie Wu <reggiewumo@gmail.com>
2017-08-25 19:30:46 +0000

    add lightbox functionality to idea image

--- resources/views/activities/common/idea.blade.php
+++ resources/views/activities/common/idea.blade.php
@@ -16,7 +16,7 @@
     @if(!$extra_images->isEmpty())
         <ul style="display:none">
             @foreach($extra_images as $image)
-                <li><a href="{{$image}}" data-imagelightbox="h"><img src="{{'/img/favicon.ico'}}"></a></li>
+                <li><a href="{{$image}}" data-imagelightbox="h"><img src="{{$image}}"></a></li>
             @endforeach
             {{--<li><a href="{{'/img/vector-map.png'}}" data-imagelightbox="h"><img src="{{'/img/favicon.ico'}}"></a></li>--}}
             {{--<li><a href="{{'/img/ElNudillo1.jpg'}}" data-imagelightbox="h"><img src="{{'/img/favicon.ico'}}"></a></li>--}}
@@ -27,7 +27,7 @@
 </div>
 @if ($idea->img_url)
     <div id="img">
-        <img src="{{ $idea->img_url }}">
+        <a href="{{ $idea->img_url }}" data-imagelightbox="j"><img src="{{ $idea->img_url }}"></a>
     </div>
     <div class="clearfix"></div>
 @endif
